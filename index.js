8b785b21a31aec1f4d1bc03c52072cc2ff83a4f7
Michael <stevemr77@gmail.com>
2021-04-18 08:42:37 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+const typeOfEvent = 'surprise';
+
+function writeCards(names, typeOfEvent){
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i ++) {
+        thankYouMessages.push(`Thank you, ${[names[i]]}, for the wonderful ${typeOfEvent} gift!`);
+    }
+    return thankYouMessages;
+}
+
+console.log(writeCards(names, typeOfEvent));
+
+let n = 10;
+function countDown(n){
+    while (n >= 0) {
+        console.log(n--);
+    }
+}
+
+countDown(5);
